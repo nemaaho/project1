86712cc96cca301e4f7f820ce85502cbe14d1182
nemaaho <nemaaho@jyu.fi>
2024-05-20 13:17:54 +0300

    testi

--- cyber_project/webapp/models.py
+++ cyber_project/webapp/models.py
@@ -1,5 +1,5 @@
 import datetime
-
+from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
 
@@ -14,6 +14,7 @@ class Question(models.Model):
 
 
 class Choice(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
